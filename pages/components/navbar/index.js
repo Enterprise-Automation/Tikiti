9498b43fecef0a48bd276bc53d34067a0ea8560a
Ethan Williams <ethanwilliams@Ethans-MacBook-Pro.local>
2021-03-29 13:37:55 +0100

    changes

--- pages/components/navbar/index.js
+++ pages/components/navbar/index.js
@@ -10,19 +10,22 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-                <Link className="navbar-brand" href="/">
-                    <Image
-                        src="/images/brand/Tikiti_Logo_Peach.png"
-                        alt="Logo"
-                        width={200}
-                        height={50}
-                    />
+                <Link href="/">
+                    <div className="navbar-brand">
+                        <Image
+                            src="/images/brand/Tikiti_Logo_Peach.png"
+                            alt="Logo"
+                            width={200}
+                            height={50}
+                        />
+                    </div>
+
                 </Link>
                 <button onClick={toggleNav} className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className={`${collapse ? "" : "collapse "}navbar-collapse`} id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul style={{ backgroundColor: "white", marginTop: "-5px", marginLeft: "-20px" }} className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <Link href="/">
                                 <a className="nav-link active" href="#">HONEYMOONS</a>
@@ -30,7 +33,7 @@ function Navbar() {
                         </li>
                         <li className="nav-item">
                             <Link href="/">
-                                <a className="nav-link " href="#">ADVENTURES</a>
+                                <a className="nav-link " href="#">LUXURY GETAWAYS</a>
                             </Link>
                         </li>
                         <li className="nav-item">
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import NavBar from './components/navbar'
+import Image from 'next/image'
 
 import { useRef, useEffect } from 'react'
 export default function Home() {
@@ -24,13 +25,65 @@ export default function Home() {
           <div className="button">BOOK NOW</div>
         </div>
       </div>
+
       <div class="text-container">
-        <div class="column">
+        <div class="column-para">
           <div class="head-row sm-2 lg-2">
             Breathtaking destinations
          </div>
           <div class="paragraph">
             From the melting pot of diverse experiences waiting to be discovered in Australia,  New Zealand &amp; the  Pacific Islands and the temples and tigers of Asia, to Americas's glittering cites and wide open spaces,  the pristine beaches and sparkling lagoons of the Indian Ocean and the sand dunes and skyscrapers of  Arabia, let us tailor-make the perfect holiday.
+          </div>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-lg images-container">
+            <div style={{ padding: "10px", position: "absolute", zIndex: "1", color: "white", fontWeight: "bold", fontFamily: "GillSans", fontSize: "30px" }}>Honeymoons</div>
+            <div className="images">
+              <Image
+                src="/images/honeymoon.jpg"
+                alt="Honeymoon"
+                width={600}
+                height={400}
+              />
+            </div>
+          </div>
+          <div class="col-lg images-container">
+            <div style={{ padding: "10px", position: "absolute", zIndex: "1", color: "white", fontWeight: "bold", fontFamily: "GillSans", fontSize: "30px" }}>Adventures</div>
+            <div className="images">
+              <Image
+                src="/images/jungle.jpg"
+                alt="Adventure"
+                width={600}
+                height={400}
+              />
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg images-container">
+            <div style={{ padding: "10px", position: "absolute", zIndex: "1", color: "white", fontWeight: "bold", fontFamily: "GillSans", fontSize: "30px" }}>Hens &amp; Stags</div>
+            <div className="images">
+              <Image
+                src="/images/spain.jpg"
+                alt="Hens"
+                width={600}
+                height={400}
+              />
+            </div>
+          </div>
+          <div class="col-lg images-container">
+            <div style={{ padding: "10px", position: "absolute", zIndex: "1", color: "white", fontWeight: "bold", fontFamily: "GillSans", fontSize: "30px" }}>Beach Breaks</div>
+            <div className="images">
+              <Image
+                src="/images/beach.jpg"
+                alt="Beach"
+                width={600}
+                height={400}
+              />
+            </div>
           </div>
         </div>
       </div>
